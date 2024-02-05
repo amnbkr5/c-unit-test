a006c558eb9a8e5348cb4d91a24c24d64fc1272d
amnbkr5 <152152565+amnbkr5@users.noreply.github.com>
2024-02-05 13:35:49 +0200

    Add files via upload

---
+++ sum/sum.c
@@ -0,0 +1,17 @@
+#include "sum.h"
+
+float sum(float arr[], unsigned int size)
+{
+    if (size == 0)
+    {
+        return 0.0;
+    }
+    
+    float total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+        total += arr[i];
+    }
+    
+    return total;
+}
---
+++ sum/sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_H
+#define SUM_H
+
+float sum(float arr[], unsigned int size);
+
+#endif // SUM_H
